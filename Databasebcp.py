f84001bf87619ea60b58632e22c15456074f29df
darkermage <darkermage@abv.bg>
2014-06-11 00:27:15 +0300

    renamed file

--- Databasebcp.py
+++ Databasebcp.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,34 +6,28 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import sessionmaker
 from Book import Book
 
-engine = create_engine('mysql://root:password@127.0.0.1:3306/bookstore', encoding='utf-8', echo = True)
+engine = create_engine('mysql://user1:123@localhost:3306/test', encoding='utf-8', echo = True)
 
 Base = declarative_base()
 
 class BookModel(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(255))
-    authors = Column(String(255))
-    ISBN = Column(String(255), unique=True)
+    title = Column(String(100))
+    authors = Column(String(100))
+    ISBN = Column(String(50), unique=True)
     pages = Column(Integer)
-    genre = Column(String(255))
-    publisher = Column(String(255))    
-    imageURL = Column(String(255))   
+    genre = Column(String(100))
+    publisher = Column(String(50))    
+    imageURL = Column(String(50))   
     grade = Column(Integer)
-    location = Column(String(255))
-    tenant = Column(String(255))
-    dateBorrow = Column(String(255))
-    dateReturned = Column(String(255))
+    location = Column(String(50))
     
-    def __init__(self, title, authors, ISBN, pages, tenant = "", dateBorrow = "", dateReturned = "", genre = "", publisher = "", imageURL = "", grade = 0, location = ""):
+    def __init__(self, title, authors, ISBN, pages, genre = "", publisher = "", imageURL = "", grade = 0, location = ""):
         self.title = title
         self.authors = authors
         self.ISBN = ISBN
         self.pages = pages
-        self.tenant = tenant
-        self.dateBorrow = dateBorrow
-        self.dateReturned = dateReturned
         self.genre = genre
         self.publisher = publisher   
         self.imageURL = imageURL     
@@ -46,16 +40,13 @@ class BookModel(Base):
         book.setAuthor(self.authors)
         book.setIsbn(self.ISBN)
         book.setNbPages(self.pages)
-        book.getTenant(self.tenant)
-        book.getDateBorrow(self.dateBorrow)
-        book.getDateReturned(self.dateReturned)
         book.setPublisher(self.publisher)     
         book.setImageURL(self.imageURL)
 
         return book
        
 def saveBook(book):
-    bookModel = BookModel(book.getTitle(), book.getAuthorsStr(), book.getIsbn(), book.getNbPages(), book.getTenant(), book.getDateBorrow(), book.getDateReturned(), "", book.getPublisher(), book.getImageURL(), 0, "")
+    bookModel = BookModel(book.getTitle(), book.getAuthorsStr(), book.getIsbn(), book.getNbPages(), "", book.getPublisher(), book.getImageURL(), 0, "")
 
     Session = sessionmaker(bind = engine)
     session = Session()
@@ -77,9 +68,6 @@ def updateBook(id, book):
     bookModel.authors = book.getAuthorsStr()
     bookModel.ISBN = book.getIsbn()
     bookModel.pages = book.getNbPages()
-    bookModel.tenant = book.getTenant()
-    bookModel.dateBorrow = book.getDateBorrow()
-    bookModel.dateReturned = book.getDateReturned()
     bookModel.genre = "" # book.genre
     bookModel.publisher = book.getPublisher()
     bookModel.imageURL = book.getImageURL()
